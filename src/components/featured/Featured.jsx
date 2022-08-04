89a8a9db28b1cef708b0b5c908bb77b084e0312f
irfan <engr.irfandayan@gmail.com>
2022-08-04 21:21:53 +0500

    Add more to charts

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 import "./featured.scss";
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text="70%" strokeWidth={5} />
+        </div>
+        <p className="title">Total Sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">
+          Previous transactions processing. Last payments may not be included.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
